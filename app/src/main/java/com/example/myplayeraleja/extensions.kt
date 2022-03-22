de0f2b88816bf4cc330b2e6f4ac5d33e3bd31746
Alejandra ACEVEDO <alejandra.acevedourrego@worldline.com>
2022-03-22 16:57:03 +0100

    feat: create extension function to create inflate a view

--- app/src/main/java/com/example/myplayeraleja/extensions.kt
+++ app/src/main/java/com/example/myplayeraleja/extensions.kt
@@ -1,7 +1,11 @@
 package com.example.myplayeraleja
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.RecyclerView
 
 
@@ -10,4 +14,9 @@ fun Context.toast(message: String, length: Int = Toast.LENGTH_SHORT) =
 
 fun RecyclerView.ViewHolder.toast(message: String) {
     itemView.context.toast(message)
+}
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int): View {
+    /* The ViewGroup is the one that gives the context for the view*/
+    return LayoutInflater.from(context).inflate(layoutRes, this, false)
 }
