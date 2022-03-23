d599060957165bb104c62156d7debbc3b0953318
Alejandra ACEVEDO <alejandra.acevedourrego@worldline.com>
2022-03-23 17:28:15 +0100

    feat: filter collection based on type selected on Menu

--- app/src/main/java/com/example/myplayeraleja/MainActivity.kt
+++ app/src/main/java/com/example/myplayeraleja/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myplayeraleja
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myplayeraleja.databinding.ActivityMainBinding
 
@@ -14,5 +16,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.recycler.adapter = mediaGridAdapter
         mediaGridAdapter.items = getItems()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.filter_all -> mediaGridAdapter.items = getItems()
+            R.id.filter_photos -> mediaGridAdapter.items = getItems().filter{ it.type == MediaItem.Type.PHOTO}
+            R.id.filter_videos -> mediaGridAdapter.items = getItems().filter{ it.type == MediaItem.Type.VIDEO}
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
