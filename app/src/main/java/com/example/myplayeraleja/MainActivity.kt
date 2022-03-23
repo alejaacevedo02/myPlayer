6d6cc82877e9d1a13e615951cd4498756b79a87b
Alejandra ACEVEDO <alejandra.acevedourrego@worldline.com>
2022-03-23 16:07:42 +0100

    refactor: practice property delegation

--- app/src/main/java/com/example/myplayeraleja/MainActivity.kt
+++ app/src/main/java/com/example/myplayeraleja/MainActivity.kt
@@ -6,16 +6,13 @@ import com.example.myplayeraleja.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private val mediaGridAdapter by lazy { MediaGridAdapter(getItems()) { toast(it.title) } }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.recycler.adapter = MediaGridAdapter(
-            getItems(),
-        ) {
-            toast(it.title)
-        }
+        binding.recycler.adapter = mediaGridAdapter
+        mediaGridAdapter.items = getItems()
     }
 }
--- app/src/main/java/com/example/myplayeraleja/MediaGridAdapter.kt
+++ app/src/main/java/com/example/myplayeraleja/MediaGridAdapter.kt
@@ -6,21 +6,26 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myplayeraleja.MediaItem.Type
 import com.example.myplayeraleja.databinding.ViewMediaItemBinding
+import kotlin.properties.Delegates
 
 class MediaGridAdapter(
-    private val mediaItems: List<MediaItem>,
+    mediaItems: List<MediaItem> = emptyList(),
     private val mediaClickedListener: (MediaItem) -> Unit
-) :
-    RecyclerView.Adapter<MediaGridAdapter.MediaViewHolder>() {
+) : RecyclerView.Adapter<MediaGridAdapter.MediaViewHolder>() {
+
+    //Modify items of the adapter using observable
+     var items: List<MediaItem> by Delegates.observable(mediaItems) { _, _, _ -> notifyDataSetChanged() }
 
     inner class MediaViewHolder(private val binding: ViewMediaItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(mediaItem: MediaItem) {
-            binding.mediaTitle.text = mediaItem.title
-            binding.mediaThumb.loadUrl(mediaItem.url)
-            binding.mediaVideoIndicator.visibility = when (mediaItem.type) {
-                Type.PHOTO -> View.GONE
-                Type.VIDEO ->  View.VISIBLE
+            with(mediaItem) {
+                binding.mediaTitle.text = title
+                binding.mediaThumb.loadUrl(url)
+                binding.mediaVideoIndicator.visibility = when (type) {
+                    Type.PHOTO -> View.GONE
+                    Type.VIDEO -> View.VISIBLE
+                }
             }
         }
     }
@@ -31,10 +36,10 @@ class MediaGridAdapter(
         return MediaViewHolder(binding)
     }
 
-    override fun getItemCount(): Int = mediaItems.size
+    override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: MediaViewHolder, position: Int) {
-        val mediaItem = mediaItems[position]
+        val mediaItem = items[position]
         holder.bind(mediaItem)
         holder.itemView.setOnClickListener {
             mediaClickedListener(mediaItem)
