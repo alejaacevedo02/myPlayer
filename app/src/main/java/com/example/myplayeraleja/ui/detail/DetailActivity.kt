b7d85467983321b5d7dce28a98e3edf0b7b15d1f
Alejandra ACEVEDO <alejandra.acevedourrego@worldline.com>
2022-06-01 17:22:56 +0200

    refactor: implement mvvm in detailActivity

--- app/src/main/java/com/example/myplayeraleja/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/example/myplayeraleja/ui/detail/DetailActivity.kt
@@ -3,11 +3,14 @@ package com.example.myplayeraleja.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import androidx.lifecycle.lifecycleScope
 import com.example.myplayeraleja.data.MediaItem
 import com.example.myplayeraleja.data.MediaItem.*
 import com.example.myplayeraleja.data.MediaProvider
 import com.example.myplayeraleja.databinding.ActivityDetailBinding
+import com.example.myplayeraleja.databinding.ActivityMainBinding
 import com.example.myplayeraleja.loadUrl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -19,27 +22,31 @@ class DetailActivity : AppCompatActivity() {
         const val EXTRA_ID = "DetailActivity:id"
     }
 
+    private lateinit var binding: ActivityDetailBinding
+    private lateinit var detailViewModel: DetailViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val itemId = intent.getIntExtra(EXTRA_ID, -1)
+        detailViewModel = ViewModelProvider(this).get()
+        initObservers()
+        detailViewModel.onCreate(itemId)
+    }
 
-        lifecycleScope.launch {
-            val items = getMediaItems()
-            val item = items.firstOrNull() { it.id == itemId }
-            item?.let {
-                supportActionBar?.title = item.title
-                binding.detailThumb.loadUrl(item.url)
-                binding.detailVideoIndicator.visibility = when (item.type) {
-                    Type.PHOTO -> View.GONE
-                    Type.VIDEO -> View.VISIBLE
-                }
-            }
+
+    private fun initObservers() {
+        detailViewModel.title.observe(this) { title ->
+            supportActionBar?.title = title
+        }
+        detailViewModel.thumb.observe(this) { url ->
+            binding.detailThumb.loadUrl(url)
+        }
+        detailViewModel.videoIndicatorVisibility.observe(this) {
+            binding.detailVideoIndicator.visibility = if (it) View.VISIBLE else View.GONE
         }
     }
-
-    private suspend fun getMediaItems(): List<MediaItem> = withContext(Dispatchers.IO) {
-        MediaProvider.getItems()
-    }
 }
+
+
---
+++ app/src/main/java/com/example/myplayeraleja/ui/detail/DetailViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.myplayeraleja.ui.detail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myplayeraleja.data.MediaItem
+import com.example.myplayeraleja.data.MediaItem.Type
+import com.example.myplayeraleja.data.MediaProvider
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class DetailViewModel : ViewModel() {
+    private val _title = MutableLiveData<String>()
+    val title: LiveData<String> get() = _title
+
+    private val _thumb = MutableLiveData<String>()
+    val thumb: LiveData<String> get() = _thumb
+
+    private val _videoIndicatorVisibility = MutableLiveData<Boolean>()
+    val videoIndicatorVisibility : LiveData<Boolean> get() = _videoIndicatorVisibility
+    
+    fun onCreate(itemId : Int) {
+        viewModelScope.launch {
+            val items = getMediaItems()
+            val item = items.firstOrNull() { it.id == itemId }
+            item?.let {
+                _title.value = item.title
+                _thumb.value = item.url
+                _videoIndicatorVisibility.value = item.type == Type.VIDEO
+                }
+            }
+        }
+
+    private suspend fun getMediaItems(): List<MediaItem> = withContext(Dispatchers.IO) {
+        MediaProvider.getItems()
+    }
+}
